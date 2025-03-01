bb83691125662288c9d73cc70c153c9efcf5a3d1
unknown <AhmadDiaz089@gmail.com>
2025-03-01 17:07:16 +0700

    add aos,shapedeviders, js to html dan perbaiki tampilan 0.4

---
+++ assets/js/aos.js
@@ -0,0 +1,12 @@
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     AOS.init({
+//         duration: 1000, // durasi animasi (ms)
+//         easing: 'ease-in-out', // jenis easing
+//         once: false, // apakah animasi hanya terjadi sekali
+//         mirror: false, // apakah animasi harus dibalik saat pengguna scroll ke atas
+//         offset: 100 // offset (dalam piksel) dari posisi trigger asli
+//     });
+//   });
